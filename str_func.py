adc2d16eca0236352819cb92737ce006249cbc8c
Roman Kondratiuk <roma.kondratiuk@icloud.com>
2023-04-01 09:34:46 -0400

    add second func

--- str_func.py
+++ str_func.py
@@ -2,4 +2,14 @@ def modified_str(str):
     new_str = str.upper()
     print(new_str)
 
+
 modified_str("roma")
+
+
+def func_2(str):
+    s = str
+    new_str = s.title()
+    print(new_str)
+
+
+func_2("roma")
