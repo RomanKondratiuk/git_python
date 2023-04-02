47a44a5007586d4e8018867b54d07351c33ecf4c
Roman Kondratiuk <roma.kondratiuk@icloud.com>
2023-04-02 14:56:54 -0400

    add docstring for second func

--- str_func.py
+++ str_func.py
@@ -7,9 +7,12 @@ modified_str("roma")
 
 
 def func_2(str):
+    """
+    Функуия , которая изменяет первую букву слова на заглавную
+    """
     s = str
     new_str = s.title()
     print(new_str)
 
 
-func_2("roma")
+func_2("roma alin")
